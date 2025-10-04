c006147aba9b4c9f6529dc70cee971af391fa13e
mif1n <artempopov_2008@mail.ru>
2025-10-04 10:30:38 +0300

    мой первый коммит)))

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,1 +1,11 @@
-print("Hello artem")
+
+number = int(input("Введите трехзначное число: "))
+if 100 <= number <= 999:
+    hundreds = number // 100  # получаем сотни
+    tens = (number // 10) % 10  # получаем десятки
+    units = number % 10  # получаем единицы
+    new_number = tens * 100 + hundreds * 10 + units
+    print(f"Исходное число: {number}")
+    print(f"Число после перестановки сотен и десятков: {new_number}")
+else:
+    print("Ошибка! Введите трехзначное число.")
