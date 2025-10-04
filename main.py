b7ce679072b41d6c4c8547c33fc4f8f735c947ed
mif1n <artempopov_2008@mail.ru>
2025-10-04 11:06:52 +0300

    мой первый комит )))))

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+try:
+    number = int(input("Введите трехзначное число: "))
+    if 100 <= number <= 999:
+        num_str = str(number)
 
+        result = int(num_str[1] + num_str[0] + num_str[2])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        print(f"{number} -> {result}")
+    else:
+        print("Ошибка: число должно быть от 100 до 999")
+except ValueError:
+    print("Ошибка: введите целое число")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
